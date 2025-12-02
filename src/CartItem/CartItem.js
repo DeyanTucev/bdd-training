86ed9303acf4f8c01d1c78da10bedede3fd3be6f
Samuel Gergely <143069755+SamuelGergely@users.noreply.github.com>
2025-12-02 13:45:36 +0100

    feat: add feature #validateArticleId

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -67,6 +67,9 @@ module.exports = class CartItem {
 
     #validateArticleId(articleId) {
         //TODO Implement this method
+        if (typeof articleId !== "number" || articleId < 1) {
+            throw new InvalidArticleIdException();
+        }
     }
 
     #validateQuantity(quantity) {
